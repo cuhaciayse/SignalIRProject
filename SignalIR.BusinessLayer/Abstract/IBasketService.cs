4e0e46f4a3c2bbfdd2377d367684010b7b5041bf
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-03-12 00:07:20 +0300

    aaaa

---
+++ SignalIR.BusinessLayer/Abstract/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using SignalIR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalIR.BusinessLayer.Abstract
+{
+    public interface IBasketService:IGenericService<Basket>
+    {
+        List<Basket> TGetBasketByMenuTableNumber(int id);
+    }
+}
---
+++ SignalIR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalIR.DataAccessLayer.Abstract;
+using SignalIR.DataAccessLayer.Concrete;
+using SignalIR.DataAccessLayer.Repositories;
+using SignalIR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalIR.DataAccessLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalIRContext context) : base(context)
+        {
+        }
+
+        public List<Basket> GetBasketByMenuTableNumber(int id)
+        {
+            using var context = new SignalIRContext();
+            var values = context.Baskets.Where(x => x.MenuTableID == id).Include(y => y.Product).ToList();
+            return values;
+        }
+    }
+}
