e3302951aa3d1c05a4bf67d8337c6bf8a54f85e6
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-03-15 21:26:51 +0300

    menu table indexler yapıldı

--- SignalIRApi/Controllers/MenuTableController.cs
+++ SignalIRApi/Controllers/MenuTableController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalIR.BusinessLayer.Abstract;
+using SignalIR.DtoLayer.MenuTableDto;
+using SignalIR.EntityLayer.Entities;
 
 namespace SignalIRApi.Controllers
 {
@@ -18,6 +20,53 @@ namespace SignalIRApi.Controllers
         public ActionResult MenuTableCount()
         {
             return Ok(_menuTableService.TMenuTableCount());
+        }
+
+        [HttpGet]
+        public ActionResult MenuTableList() 
+        {
+            var values = _menuTableService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+			MenuTable menuTable = new MenuTable()
+			{
+				Name = createMenuTableDto.Name,
+				Status = false
+			};
+			_menuTableService.TAdd(menuTable);
+			return Ok("Masa eklendi");
+		}
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            _menuTableService.TDelete(value);
+            return Ok("Masa silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            MenuTable menuTable = new MenuTable()
+            {
+                Name = updateMenuTableDto.Name,
+                Status = false,
+                MenuTableID = updateMenuTableDto.MenuTableID
+            };
+            _menuTableService.TUpdate(menuTable);
+            return Ok("Masa bilgisi güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            return Ok(value);   
         }
     }
 }
