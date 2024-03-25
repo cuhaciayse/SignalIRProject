f28682e19898ce294e6f92ba63bbd904d28a7b3b
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-03-26 01:12:09 +0300

    aaaa

---
+++ SignalIRWebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace SignalRWebUI.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string value)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(10); // QR kodu görüntüsünü al
+                qrCodeImage.Save(memoryStream, ImageFormat.Png);
+                ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+            }
+            return View();
+        }
+    }
+}
